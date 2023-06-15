3fe9275aecb75b9e616992ff5b1db07c0e2b768a
mmkuznecov <mmkuznecov2002@gmail.com>
2023-06-15 22:05:25 +0300

    added configs

--- faceblur/tests/test_blurring.py
+++ faceblur/tests/test_blurring.py
@@ -1,10 +1,11 @@
 import requests
 import time
 import base64
+from testing_utils import read_config
 
 
-def send_image_for_blurring(image_filename):
-    url = "http://localhost:8000/blur_faces"
+def send_image_for_blurring(image_filename, api_url):
+    url = f"http://{api_url}/blur_faces"
     mime_type = 'image/jpeg'
     with open(image_filename, 'rb') as image_file:
         files = {
@@ -19,8 +20,8 @@ def send_image_for_blurring(image_filename):
         return None
 
 
-def get_task_result(task_id):
-    url = f"http://localhost:8000/task_result/{task_id}"
+def get_task_result(task_id, api_url):
+    url = f"http://{api_url}/task_result/{task_id}"
     response = requests.get(url)
     if response.status_code == 200:
         return response.json()
@@ -30,18 +31,22 @@ def get_task_result(task_id):
 
 
 def main():
-    image_filename = "test_imgs/test.jpg"
-    task_id = send_image_for_blurring(image_filename)
+    config = read_config('test_config.yaml')
+
+    image_filename = config['test_img_path']
+    api_url = config['api_url']
+
+    task_id = send_image_for_blurring(image_filename, api_url)
 
     if task_id:
         print(f"Submitted image for blurring. Task ID: {task_id}")
-        result = get_task_result(task_id)
+        result = get_task_result(task_id, api_url)
 
         # Poll for result until task is done
         while result['status'] != 'SUCCESS':
             print("Task not ready yet. Waiting for 1 second...")
             time.sleep(1)
-            result = get_task_result(task_id)
+            result = get_task_result(task_id, api_url)
 
         if result['status'] == 'SUCCESS':
             print("Task finished. Saving result to file...")
--- faceblur/tests/test_detection.py
+++ faceblur/tests/test_detection.py
@@ -1,9 +1,10 @@
 import requests
 import time
+from testing_utils import read_config
 
 
-def send_image_for_detection(image_filename):
-    url = "http://localhost:8000/detect_faces"
+def send_image_for_detection(image_filename, api_url):
+    url = f"http://{api_url}/detect_faces"
     mime_type = 'image/jpeg'
     with open(image_filename, 'rb') as image_file:
         files = {
@@ -18,8 +19,8 @@ def send_image_for_detection(image_filename):
         return None
 
 
-def get_task_result(task_id):
-    url = f"http://localhost:8000/task_result/{task_id}"
+def get_task_result(task_id, api_url):
+    url = f"http://{api_url}/task_result/{task_id}"
     response = requests.get(url)
     if response.status_code == 200:
         return response.json()
@@ -29,18 +30,23 @@ def get_task_result(task_id):
 
 
 def main():
-    image_filename = "test_imgs/test.jpg"
-    task_id = send_image_for_detection(image_filename)
+
+    config = read_config('test_config.yaml')
+
+    image_filename = config['test_img_path']
+    api_url = config['api_url']
+
+    task_id = send_image_for_detection(image_filename, api_url)
 
     if task_id:
         print(f"Submitted image for detection. Task ID: {task_id}")
-        result = get_task_result(task_id)
+        result = get_task_result(task_id, api_url)
 
         # Poll for result until task is done
         while result['status'] != 'SUCCESS':
             print("Task not ready yet. Waiting for 0.1 second...")
             time.sleep(0.1)
-            result = get_task_result(task_id)
+            result = get_task_result(task_id, api_url)
 
         if result['status'] == 'SUCCESS':
             print("Task finished. Result:")
