c95c85abeb811b6335472d27ddbb4da73885d7ac
mmkuznecov <mmkuznecov2002@gmail.com>
2023-06-15 22:17:10 +0300

    added docstrings

--- faceblur/face_detection_service/worker.py
+++ faceblur/face_detection_service/worker.py
@@ -3,7 +3,6 @@ from cv.utils import detection_processing, blurring_processing
 from cv.yolov8 import YOLOv8Face
 from utils import read_config
 
-
 # Initiate Celery
 celery_app = Celery('face_detection',
                     broker='pyamqp://guest@rabbitmq//',
@@ -19,11 +18,34 @@ model = YOLOv8Face(MODEL_PATH)
 
 @celery_app.task(name='detect_faces_task')
 def detect_faces_task(image_data):
+    """
+    This function receives image data as a parameter,
+    performs face detection on it using a pre-trained YOLOv8Face
+    model and returns the bounding boxes of detected faces
+    in JSON format.
+
+    Parameters:
+    - image_data: Image data to be processed.
+
+    Returns:
+    - JSON bounding boxes of detected faces.
+    """
     json_bboxes = detection_processing(image_data, model)
     return json_bboxes
 
 
 @celery_app.task(name='blur_faces_task')
 def blur_faces_task(image_data):
+    """
+    This function receives image data as a parameter,
+    blurs the faces detected in the image using a pre-trained
+    YOLOv8Face model and returns the processed image.
+
+    Parameters:
+    - image_data: Image data to be processed.
+
+    Returns:
+    - The processed image with faces blurred.
+    """
     encoded_image = blurring_processing(image_data, model)
     return encoded_image
--- faceblur/gateway_service/app.py
+++ faceblur/gateway_service/app.py
@@ -13,6 +13,17 @@ celery_app = Celery('gateway_service',
 
 @app.post("/detect_faces")
 async def detect_faces(file: UploadFile = File(...)):
+    """
+    Endpoint to receive an image file,
+    perform face detection and return a task ID.
+
+    Parameters:
+    - file: Image file uploaded by the user.
+
+    Returns:
+    - A dictionary containing a 'task_id' key
+    with the ID of the face detection task.
+    """
     image_data = await file.read()
     base64_image = base64.b64encode(image_data).decode('utf-8')
     task = celery_app.send_task("detect_faces_task", args=[base64_image])
@@ -21,6 +32,17 @@ async def detect_faces(file: UploadFile = File(...)):
 
 @app.post("/blur_faces")
 async def blur_faces(file: UploadFile = File(...)):
+    """
+    Endpoint to receive an image file,
+    blur the faces in the image, and return a task ID.
+
+    Parameters:
+    - file: Image file uploaded by the user.
+
+    Returns:
+    - A dictionary containing a 'task_id' key
+    with the ID of the face blurring task.
+    """
     image_data = await file.read()
     base64_image = base64.b64encode(image_data).decode('utf-8')
     task = celery_app.send_task('blur_faces_task', args=[base64_image])
@@ -29,6 +51,17 @@ async def blur_faces(file: UploadFile = File(...)):
 
 @app.get("/task_result/{task_id}")
 def get_task_result(task_id: str):
+    """
+    Endpoint to get the result of a specific task based on the task ID.
+
+    Parameters:
+    - task_id: ID of the task whose result is to be fetched.
+
+    Returns:
+    - A dictionary containing 'status' key indicating the status of the task.
+    If the task is successful,
+    it also contains a 'result' key with the task's result.
+    """
     task = AsyncResult(task_id, app=celery_app)
     if task.successful():
         result = task.result
